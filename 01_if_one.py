cd26afbd05a844b8fdb110595b9e964b36f07e95
Aditya Ramteke <adityaramteke05icr@gmail.com>
2017-11-25 10:49:46 +0530

    Sample Programs

--- 01_if_one.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter sample string :")
-if len(name) < 6:
-    print("Your string is too short")
-    print("Please enter at least 6 characters")
--- 02_if_two.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Please enter your number: "))
-if num%2 == 0:
-    print("Your number is even.")
-else:
-    print("Your number is odd.")
