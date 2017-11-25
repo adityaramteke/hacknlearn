5f990a1c9ce3cd638c2d2167f029eaefb966a4b1
Aditya Ramteke <adityaramteke05icr@gmail.com>
2017-11-25 13:57:12 +0530

    Created directory functions .

---
+++ functions/01_volume_shpere.py
@@ -0,0 +1,10 @@
+
+import math
+
+def volume(r):
+    """ Volume of shpere having radius r. """
+    v = (4.0 / 3.0) * math.pi * r**3
+    return v
+
+print(volume(2))    # Volume of sphere having radius 2
+
